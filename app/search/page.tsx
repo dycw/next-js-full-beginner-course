e0e9ef17dcf48802a2427f232a81533c1c0a1026
Derek Wan <d.wan@icloud.com>
2023-06-02 20:44:45 +0800

    Move to non-src, export metadata the modern way

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,9 @@
-export default function SearchNewsPage() {
+const SearchNewsPage = () => {
   return (
     <main>
       <h1>Search News</h1>
     </main>
   );
-}
+};
+
+export default SearchNewsPage;

7a44fe05dda46198e1e1f31d6665a73d8fab9ea6
Derek Wan <d.wan@icloud.com>
2023-06-03 10:20:23 +0800

    Finish images, remove submodule (#7)
    * Finish images
    
    * Remove submodule

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,4 +1,7 @@
+import placeholderImage from "../assets/images/newsarticle_placeholder.jpg";
 import { NewsArticle } from "../models/NewsArticles";
+import styles from "../styles/NewsArticleEntry.module.css";
+import Image from "next/image";
 import { Card } from "react-bootstrap";
 
 type NewsArticleEntryProps = {
@@ -15,7 +18,13 @@ export default function NewsArticleEntry({
   return (
     <a href={url}>
       <Card className="h-100">
-        <Card.Img variant="top" src={validImageUrl}></Card.Img>
+        <Image
+          src={validImageUrl || placeholderImage}
+          width={500}
+          height={200}
+          alt=""
+          className={`card-img-top ${styles.image}`}
+        />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
